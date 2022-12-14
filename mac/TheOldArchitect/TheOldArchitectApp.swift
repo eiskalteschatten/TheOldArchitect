0c93d4d33da2b63f5c7eb124638ba93cb28a3665
Alex Seifert <alex@alexseifert.com>
2022-12-14 22:59:44 +0100

    Initial commit

---
+++ mac/TheOldArchitect/TheOldArchitectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOldArchitectApp.swift
+//  TheOldArchitect
+//
+//  Created by Alex Seifert on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TheOldArchitectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
