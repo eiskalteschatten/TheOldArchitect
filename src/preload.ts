0c398962e32a9056b8689901139ba7284f294204
Alex Seifert <alex@alexseifert.com>
2022-12-15 12:25:08 +0100

    Add the APIs to the context bridge

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,30 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer, shell } from 'electron';
+
+contextBridge.exposeInMainWorld(
+  'api',
+  {
+    send: ipcRenderer.send,
+    sendSync: ipcRenderer.sendSync,
+    on: (channel: any, callback: any): void => {
+      ipcRenderer.on(channel, callback);
+    },
+    once: (channel: any, callback: any): void => {
+      ipcRenderer.once(channel, callback);
+    },
+    removeListener: (channel: any, callback: any): void => {
+      ipcRenderer.removeListener(channel, callback);
+    },
+    removeAllListeners: (channel: any): void => {
+      ipcRenderer.removeAllListeners(channel);
+    },
+  }
+);
+
+contextBridge.exposeInMainWorld(
+  'shell',
+  {
+    openExternal: (url: string): Promise<void> => shell.openExternal(url),
+  }
+);
